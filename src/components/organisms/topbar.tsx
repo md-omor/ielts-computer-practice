a34d1b220cae19a2bc65c41abfe1840aae38df35
Md Omor <developeromor@gmail.com>
2025-02-19 22:00:16 +0600

    add topbar

---
+++ src/components/organisms/topbar.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Timer from "@/components/molecules/timer";
+import UserNumber from "@/components/molecules/user-number";
+
+import HelpDialog from "@/features/topbar/help-dialog";
+import HideDialog from "@/features/topbar/hide-dialog";
+import SettingsDialog from "@/features/topbar/settings-dialog";
+import { useState } from "react";
+import { PiQuestionMarkFill } from "react-icons/pi";
+import Sound from "../atoms/sound";
+import { Button } from "../ui/button";
+
+const Topbar = () => {
+  const [activeDialog, setActiveDialog] = useState<
+    "settings" | "help" | "hide" | null
+  >(null);
+
+  const handleCloseDialog = () => setActiveDialog(null);
+
+  return (
+    <>
+      <div className="flex justify-between items-center bg-gray-800 text-white py-2 px-10">
+        <UserNumber />
+        <Timer />
+        <div className="flex items-center gap-x-2">
+          <Button
+            className="!py-0 !px-3 !h-8 !text-sm font-medium"
+            onClick={() => setActiveDialog("settings")}
+          >
+            Settings
+          </Button>
+          <Button
+            className="!py-0 !px-3 !h-8 !text-sm font-medium"
+            onClick={() => setActiveDialog("help")}
+          >
+            Help <PiQuestionMarkFill className="text-slate-600" />{" "}
+          </Button>
+          <Button
+            className="!py-0 !px-3 !h-8 !text-sm font-medium"
+            onClick={() => setActiveDialog("hide")}
+          >
+            Hide
+          </Button>
+          <Sound />
+        </div>
+      </div>
+
+      <SettingsDialog
+        isOpen={activeDialog === "settings"}
+        onClose={handleCloseDialog}
+      />
+
+      <HelpDialog
+        isOpen={activeDialog === "help"}
+        onClose={handleCloseDialog}
+      />
+
+      <HideDialog
+        isOpen={activeDialog === "hide"}
+        onClose={handleCloseDialog}
+      />
+    </>
+  );
+};
+
+export default Topbar;
