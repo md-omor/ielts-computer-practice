a98cf338a8db458f818ea11aa3c4eff4b38e8b51
Md Omor <developeromor@gmail.com>
2025-02-19 22:00:10 +0600

    add user number

---
+++ src/components/molecules/user-number.tsx
@@ -0,0 +1,12 @@
+import { FaUser } from "react-icons/fa";
+
+const UserNumber = () => {
+  return (
+    <div className="flex gap-x-2 items-center">
+      <FaUser size={20} />
+      <h1 className="text-base">XXXXX {"  "} XXXXXXXX - 123456</h1>
+    </div>
+  );
+};
+
+export default UserNumber;
