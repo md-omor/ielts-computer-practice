3f4334a9dc95c1c8ce463b5f2a24bfb76b927283
Md Omor <developeromor@gmail.com>
2025-02-20 23:05:36 +0600

    solve build error

--- src/components/molecules/ExamInstructions.tsx
+++ src/components/molecules/ExamInstructions.tsx
@@ -1,4 +1,5 @@
-import { ExamType, startTest } from "@/store/slices/timerSlice";
+import type { ExamType } from "@/store/slices/timerSlice";
+import { startTest } from "@/store/slices/timerSlice";
 import type { RootState } from "@/store/store";
 import { getTextSizeClass } from "@/utils/textSizeUtils";
 import { Info } from "lucide-react";
