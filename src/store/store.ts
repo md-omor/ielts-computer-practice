334caba317bad31c46fe07d9dcc84d0e42bc6272
Md Omor <developeromor@gmail.com>
2025-02-20 09:59:40 +0600

    add redux presist for sotring data

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,34 @@
 "use client";
 
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import settingsReducer from "./slices/settingsSlice";
 
-export const store = configureStore({
-  reducer: {
-    settings: settingsReducer,
-  },
+const rootReducer = combineReducers({
+  settings: settingsReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["settings"],
+  debug: true,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
