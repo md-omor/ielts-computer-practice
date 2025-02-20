6ed6e111735710819177d026f34f45fd4a6c46d9
Md Omor <developeromor@gmail.com>
2025-02-20 21:26:20 +0600

    edit listening

--- src/layouts/listening/listening.tsx
+++ src/layouts/listening/listening.tsx
@@ -1,6 +1,6 @@
 const ListeningLayout = () => {
   return (
-    <div>
+    <div className="w-full h-full px-5 py-6 bg-gradient-to-b from-sky-50 via-white to-sky-100">
       <h1>asdds</h1>
     </div>
   );
