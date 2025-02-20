c727ab4fe8bdf895b9b97049b7d81dcdbc397e46
Md Omor <developeromor@gmail.com>
2025-02-20 09:34:16 +0600

    edit exam layout

--- src/app/exam/layout.tsx
+++ src/app/exam/layout.tsx
@@ -1,16 +1,23 @@
-import Topbar from "@/components/organisms/topbar";
+"use client";
 
-export default function Listening({
+import Topbar from "@/components/organisms/topbar";
+import type { RootState } from "@/store/store";
+import { getTextSizeClass } from "@/utils/textSizeUtils";
+import { useSelector } from "react-redux";
+
+export default function ExamLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { textSize } = useSelector((state: RootState) => state.settings);
+
   return (
-    <html lang="en">
-      <body className={`antialiased`}>
-        <Topbar />
+    <>
+      <Topbar />
+      <div className={`${getTextSizeClass("text-sm", textSize)}`}>
         {children}
-      </body>
-    </html>
+      </div>
+    </>
   );
 }
