b20140e993adedf562beaeeed9d7e8ad39758465
Md Omor <developeromor@gmail.com>
2025-02-20 09:34:26 +0600

    edit exam page

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import ExamInstructions from "@/components/molecules/ExamInstructions";
+import { examConfigurations } from "@/config/examData";
+import { useRouter } from "next/navigation";
+
 const Exam = () => {
+  const router = useRouter();
+  const examType = "Listening"; // This could be dynamic based on your needs
+  const examData = examConfigurations[examType];
+
+  const handleStartTest = () => {
+    router.push(examData.route);
+  };
+
   return (
-    <div>
-      <h1 className="">asdd</h1>
+    <div className="w-full px-5 py-6 bg-gradient-to-b from-sky-50 via-white to-sky-100 h-screen">
+      <ExamInstructions {...examData} onStart={handleStartTest} />
     </div>
   );
 };
