01c306b83c4a4bc3609cf7a7a0ea9d75bbd82a28
Md Omor <developeromor@gmail.com>
2025-02-20 09:39:54 +0600

    add exam data

---
+++ src/config/examData.ts
@@ -0,0 +1,73 @@
+import { ExamType } from "@/components/molecules/ExamInstructions";
+
+interface ExamConfig {
+  examType: ExamType;
+  duration: string;
+  instructions: string[];
+  information: string[];
+  route: string;
+}
+
+export const examConfigurations: Record<ExamType, ExamConfig> = {
+  Listening: {
+    examType: "Listening",
+    duration: "Approximately 30 minutes",
+    instructions: [
+      "Answer all the questions.",
+      "You can change your answers at any time during the test.",
+    ],
+    information: [
+      "There are 40 questions in this test.",
+      "Each question carries one mark.",
+      "There are four parts to the test.",
+      "You will hear each part once.",
+      "For each part of the test there will be time for you to look through the questions and time for you to check your answers.",
+    ],
+    route: "/exam/listening",
+  },
+  Reading: {
+    examType: "Reading",
+    duration: "60 minutes",
+    instructions: [
+      "Answer all the questions.",
+      "You can change your answers at any time during the test.",
+    ],
+    information: [
+      "There are 40 questions in this test.",
+      "Each question carries one mark.",
+      "There are three sections to the test.",
+      "The texts become progressively more difficult.",
+    ],
+    route: "/exam/reading",
+  },
+  Writing: {
+    examType: "Writing",
+    duration: "60 minutes",
+    instructions: [
+      "Answer both tasks.",
+      "You can change your answers at any time during the test.",
+    ],
+    information: [
+      "There are 2 tasks in this test.",
+      "You should spend about 20 minutes on Task 1.",
+      "You should spend about 40 minutes on Task 2.",
+      "Task 2 contributes twice as much as Task 1 to the Writing score.",
+    ],
+    route: "/exam/writing",
+  },
+  Speaking: {
+    examType: "Speaking",
+    duration: "11-14 minutes",
+    instructions: [
+      "Answer all questions.",
+      "Speak clearly into the microphone.",
+    ],
+    information: [
+      "The test is in three parts.",
+      "Part 1: Introduction and interview.",
+      "Part 2: Individual long turn.",
+      "Part 3: Two-way discussion.",
+    ],
+    route: "/exam/speaking",
+  },
+};
