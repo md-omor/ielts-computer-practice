aca62c0ac4be41046c378eb01b1b7c8f1ef375bb
Md Omor <developeromor@gmail.com>
2025-02-20 21:25:31 +0600

    edit exam layout

--- src/app/exam/layout.tsx
+++ src/app/exam/layout.tsx
@@ -1,23 +1,39 @@
 "use client";
 
 import Topbar from "@/components/organisms/topbar";
-import type { RootState } from "@/store/store";
+import { useAppSelector } from "@/hooks/reduxHooks";
 import { getTextSizeClass } from "@/utils/textSizeUtils";
-import { useSelector } from "react-redux";
 
 export default function ExamLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { textSize } = useSelector((state: RootState) => state.settings);
+  const { textSize, screenResolution } = useAppSelector(
+    (state) => state.settings
+  );
+  const [width, height] = screenResolution.split("x").map(Number);
 
   return (
-    <>
-      <Topbar />
-      <div className={`${getTextSizeClass("text-base", textSize)}`}>
-        {children}
+    <div
+      data-route="exam"
+      className="flex flex-col items-center min-h-screen bg-gray-100"
+    >
+      <div style={{ width: `${width}px` }}>
+        <Topbar />
+        <div
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+          }}
+          className={`bg-white overflow-hidden ${getTextSizeClass(
+            "text-base",
+            textSize
+          )}`}
+        >
+          {children}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
